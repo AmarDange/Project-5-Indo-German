57ec7925bd0bf2ba58680b8a3f441fbeace65ec1
AmarDange <amardange218@gmail.com>
2023-06-21 15:38:15 +0000

    addng CSS file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import Button from "react-bootstrap/Button";
-import './App.css';
+// import logo from './logo.svg';
+// import Button from "react-bootstrap/Button";
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Button variant="primary">Primary</Button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
     </div>
   );
 }
