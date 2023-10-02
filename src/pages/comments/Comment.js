b01f63425c6dbbb544e3410b17759d381244111b
AmarDange <>
2023-10-02 09:22:50 +0000

    adding functionality for comment like

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -18,6 +18,8 @@ const Comment = (props) => {
     id,
     setPost,
     setComments,
+    comment_likes_id,
+    comment_likes_count,
   } = props;
 
   const [showEditForm, setShowEditForm] = useState(false);
@@ -43,22 +45,136 @@ const Comment = (props) => {
     } catch (err) { }
   };
 
+//   return (
+//     <>
+//       <hr />
+//       <Media>
+//         <Link to={`/profiles/${profile_id}`}>
+//           <Avatar src={profile_image} />
+//         </Link>
+//         <Media.Body className="align-self-center ml-2">
+//           <span className={styles.Owner}>{owner}</span>
+//           <span className={styles.Date}>{updated_at}</span>
+//           {showEditForm ? (
+//             <CommentEditForm
+//               id={id}
+//               profile_id={profile_id}
+//               content={content}
+//               profileImage={profile_image}
+//               setComments={setComments}
+//               setShowEditForm={setShowEditForm}
+//             />
+//           ) : (
+//             <p>{content}</p>
+//           )}
+//         </Media.Body>
+//         {is_owner && !showEditForm && (
+//           <MoreDropdown
+//             handleEdit={() => setShowEditForm(true)}
+//             handleDelete={handleDelete}
+//           />
+//         )}
+//       </Media>
+//     </>
+//   );
+// };
+
+// export default Comment;
+
+const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/comment_likes/", { comment: id });
+    //   console.log("data",data);
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                comment_likes_count: comment.comment_likes_count + 1,
+                comment_likes_id: data.id,
+              }
+            : comment;
+
+        }),
+         }));
+        //  console.log("Comments likes count", comment_likes_count);
+    } catch (err) {
+    //   console.log(err);
+    }
+  };
+  /**
+   * To unlike a comment by the user who liked it
+   * Sends a request to the API
+   * Decreases likes number by 1
+   */
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/comment_likes/${comment_likes_id}`);
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                comment_likes_count: comment.comment_likes_count - 1,
+                comment_likes_id: null,
+              }
+            : comment;
+        }),
+      }));
+    } catch (err) {
+    //   console.log(err);
+    }
+  };
+
   return (
     <>
       <hr />
       <Media>
-        <Link to={`/profiles/${profile_id}`}>
+        <Link
+          to={`/profiles/${profile_id}`}
+          aria-label="Click here to go to profile page"
+        >
           <Avatar src={profile_image} />
         </Link>
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
-          <span className={styles.Date}>{updated_at}</span>
+          <span className={styles.Date}>{updated_on}</span>
+          <div className=" text-right">
+            {isOwner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't like your own comments!</Tooltip>}
+              >
+                <i className="far fa-heart" />
+              </OverlayTrigger>
+            ) : comment_likes_id ? (
+              <span onClick={handleUnlike}>
+                <i className={`fas fa-heart ${styles.Heart}`}></i>
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleLike}>
+                <i className={`far fa-heart ${styles.HeartOutline}`} />
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to like comments!</Tooltip>}
+              >
+                <i className="far fa-heart" />
+              </OverlayTrigger>
+            )}
+            {comment_likes_count}
+          </div>
           {showEditForm ? (
             <CommentEditForm
               id={id}
               profile_id={profile_id}
               content={content}
+              post={post}
               profileImage={profile_image}
+              updated_on={updated_on}
               setComments={setComments}
               setShowEditForm={setShowEditForm}
             />
@@ -66,12 +182,18 @@ const Comment = (props) => {
             <p>{content}</p>
           )}
         </Media.Body>
-        {is_owner && !showEditForm && (
-          <MoreDropdown
+        {/* Display the dropdown menu for owner of the comment
+                                    to either edit or delete it */}
+        {isOwner && !showEditForm && (
+          <DropdownMenu
+                handleDeleteClick={handleDeleteClick}
             handleEdit={() => setShowEditForm(true)}
-            handleDelete={handleDelete}
           />
+        
         )}
+        {showDeleteModal && (
+                    <DeleteModal onDelete={handleDelete} onCancel={handleCancel} />
+                  )}
       </Media>
     </>
   );
